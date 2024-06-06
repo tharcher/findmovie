00e3f37c20c287a57ac702a49b7b74242b058545
tharcher <thaisramos12@hotmail.com>
2024-06-06 16:57:55 -0300

    Inicializando o projeto e frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { HeaderTitle } from "./components/Header/HeaderTitle"
+
+function App() {
+
+  return (
+    <>
+      <HeaderTitle />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        evergreen: '#284341',
+        'evergreen-light': '#2AA780',
+      }
+    },
+  },
+  plugins: [],
+}
+
