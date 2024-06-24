b5c4cbde5ffe2c4af6f2acfd4d9f096efe4d953e
tharcher <thaisramos12@hotmail.com>
2024-06-24 02:22:53 -0300

    fazendo um pageup automatico ao trocar de pagina

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { MovieDetails } from "../pages/MovieDetails";
+import { useEffect } from "react";
 
 export const AppRoutes = () => {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:id" element={<MovieDetails/>} />
             </Routes>
         </BrowserRouter>
     );
+};
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
 };
